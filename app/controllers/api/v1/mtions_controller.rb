b107f634de8d4b061dfa1dc6533612c4fe5a0476
PritiKumr <priti922474@gmail.com>
2016-04-28 00:45:44 +0530

    Fix

--- app/controllers/api/v1/mtions_controller.rb
+++ app/controllers/api/v1/mtions_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::MtionsController < Api::V1::ApplicationController
 
 	def get_value
 		a = Motion.new
-		a.x_axis = params[:x]
-		a.y_axis = params[:y]
-		a.z_axis = params[:z]
+		a.x_axis = params[:x_axis]
+		a.y_axis = params[:y_axis]
+		a.z_axis = params[:z_axis]
 		a.save
 		render json: { message: 'Value updated.', status: 200 }, status: 200
 	end
